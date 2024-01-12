62fbf194de830c97ce9f5833008972a7cb0be267
wetarorg <tarframework@gmail.com>
2024-01-12 20:00:03 +0530

    s1

---
+++ astro.config.mjs
@@ -0,0 +1,31 @@
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+import tailwind from "@astrojs/tailwind";
+
+import cloudflare from "@astrojs/cloudflare";
+
+// https://astro.build/config
+export default defineConfig({
+    integrations: [starlight({
+    title: 'My Docs',
+    social: {
+      github: 'https://github.com/withastro/starlight'
+    },
+    sidebar: [{
+      label: 'Guides',
+      items: [
+      // Each item here is one entry in the navigation menu.
+      {
+        label: 'Example Guide',
+        link: '/guides/example/'
+      }]
+    }, {
+      label: 'Reference',
+      autogenerate: {
+        directory: 'reference'
+      }
+    }]
+  }), tailwind()],
+  output: 'server',
+  adapter: cloudflare()
+});
