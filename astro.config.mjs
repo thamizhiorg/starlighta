294c650ff929a752d4e198d20ee337c3002fbff5
thamizhi <153293252+thamizhiorg@users.noreply.github.com>
2024-02-04 17:23:39 +0000

    ten steps

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,31 +1,82 @@
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 import tailwind from "@astrojs/tailwind";
 
 import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [starlight({
-    title: 'My Docs',
-    social: {
-      github: 'https://github.com/withastro/starlight'
-    },
-    sidebar: [{
-      label: 'Guides',
-      items: [
-      // Each item here is one entry in the navigation menu.
-      {
-        label: 'Example Guide',
-        link: '/guides/example/'
-      }]
-    }, {
-      label: 'Reference',
-      autogenerate: {
-        directory: 'reference'
-      }
-    }]
-  }), tailwind()],
-  output: 'hybrid',
+  integrations: [
+    starlight({
+      title: "Structural Tamil Genocide",
+      social: {
+        twitter: "https://github.com/withastro/starlight",
+      },
+      sidebar: [
+        {
+          label: "CLASSIFICATION",
+          autogenerate: {
+            directory: "classification",
+          },
+        },
+        {
+          label: "SYMBOLIZATION",
+          autogenerate: {
+            directory: "SYMBOLIZATION",
+          },
+        },
+        {
+          label: "DISCRIMINATION",
+          autogenerate: {
+            directory: "SYMBOLIZATION",
+          },
+        },
+        {
+          label: "DEHUMANIZATION",
+          autogenerate: {
+            directory: "SYMBOLIZATION",
+          },
+        },
+        {
+          label: "ORGANIZATION",
+          autogenerate: {
+            directory: "DISCRIMINATION",
+          },
+        },
+        {
+          label: "POLARIZATION",
+          autogenerate: {
+            directory: "DISCRIMINATION",
+          },
+        },
+        {
+          label: "PREPARATION",
+          autogenerate: {
+            directory: "DISCRIMINATION",
+          },
+        },
+        {
+          label: "PERSECUTION",
+          autogenerate: {
+            directory: "DISCRIMINATION",
+          },
+        },
+        {
+          label: "EXTERMINATION",
+          autogenerate: {
+            directory: "DISCRIMINATION",
+          },
+        },
+        {
+          label: "DENIAL",
+          autogenerate: {
+            directory: "DISCRIMINATION",
+          },
+        },
+      ],
+    }),
+    tailwind(),
+  ],
+  output: "hybrid",
   adapter: cloudflare(),
 });
